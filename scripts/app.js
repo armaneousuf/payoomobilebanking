a1e22d133b47e01e7de876d9f97c5407d7bb8040
armaneousuf <armaneousuf97@gmail.com>
2026-01-19 23:01:12 +0600

    feat: added favicon and adEventListener

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+// Homepage login button functionality
+
+document.querySelector('#login-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log("Button clicked");
+})
