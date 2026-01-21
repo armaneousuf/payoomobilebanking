f5e23cc527e9b6563eed11fd23bac590826ae8cd
armaneousuf <armaneousuf97@gmail.com>
2026-01-21 12:57:24 +0600

    feat: completed login script and design

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,22 @@
 // Homepage login button functionality
 
+const mobileNumber = 1234567890;
+const pinNumber = 1234;
 document.querySelector('#login-btn').addEventListener('click', function (e) {
     e.preventDefault();
-    console.log("Button clicked");
+    
+    const mobileNumberValue = document.querySelector('#mobile').value.trim();
+    const pinNumberValue = document.querySelector("#password").value.trim();
+
+    const mobileNumberValueConverter = parseInt(mobileNumberValue);
+    const pinNumberValueConverter = parseInt(pinNumberValue);
+
+    // console.log(mobileNumberValueConverter);
+
+    if (mobileNumberValueConverter === mobileNumber && pinNumberValueConverter === pinNumber) {
+        window.location.href='./home.html'
+    } else {
+        alert('invalid credential. Use 1234567890 and 1234');
+    }
+    
 })
