e818efc1938ba5abbc8e4c39c9319cf57800f600
armaneousuf <armaneousuf97@gmail.com>
2026-01-23 18:54:02 +0600

    feat: completed add money functionality

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,19 @@
-
-
-document.querySelector("#add-money-btn").addEventListener('click', function (e) {
+document
+  .querySelector("#add-money-btn")
+  .addEventListener("click", function (e) {
     e.preventDefault();
-    
-    const bank = document.querySelector('#bank-select').value;
-    const accountNumber = document.querySelector('#bank-num').value;
+
+    const bank = document.querySelector("#bank-select").value;
+    const accountNumber = document.querySelector("#bank-num").value;
     const amount = parseFloat(document.querySelector("#add-amount").value);
-    const pin = document.querySelector('#password').value;
+    const pin = document.querySelector("#password").value;
+    const totalAmount = parseFloat(
+      document.querySelector("#total-amount").innerText,
+      );
 
-    console.log(bank, accountNumber, amount, pin);
+      const totalNewBalance = amount + totalAmount;
+      
+      document.querySelector('#total-amount').innerText = totalNewBalance;
 
-})
+    
+  });
