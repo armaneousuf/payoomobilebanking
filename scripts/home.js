1d3eb720dab3f018c20d55e9d307edd92fa89e52
armaneousuf <armaneousuf97@gmail.com>
2026-01-23 17:52:10 +0600

    feat: added addEvent for button and variables

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+
+
+document.querySelector("#add-money-btn").addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const bank = document.querySelector('#bank-select').value;
+    const accountNumber = document.querySelector('#bank-num').value;
+    const amount = parseFloat(document.querySelector("#add-amount").value);
+    const pin = document.querySelector('#password').value;
+
+    console.log(bank, accountNumber, amount, pin);
+
+})
