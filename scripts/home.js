aefbaa4dcdcc766c75c4c01f8a4b69e1f358c03f
armaneousuf <armaneousuf97@gmail.com>
2026-02-01 14:39:07 +0600

    feat: added new design for other forms and refoactor previous code without changing main logic

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,6 @@
 // Add Money Logics
 const pinNumber = 1234;
 
-
 // Reusable function
 
 function getInputValue(id) {
@@ -12,6 +11,24 @@ function getInputNumber(id) {
   return parseFloat(document.querySelector(id).value);
 }
 
+function getInnerText(id) {
+  return parseFloat(document.querySelector(id).innerText);
+}
+
+function setInnerText(id, text) {
+  document.querySelector(id).innerText = text;
+}
+
+function handleToggle(id) {
+  const forms = document.querySelectorAll(".form");
+
+  for (const form of forms) {
+    form.style.display = "none";
+  }
+
+  document.querySelector(id).style.display = "block";
+}
+
 document
   .querySelector("#add-money-btn")
   .addEventListener("click", function (e) {
@@ -19,11 +36,9 @@ document
 
     const bank = getInputValue("#bank-select");
     const accountNumber = getInputValue("#bank-num");
-    const amount = getInputNumber('#add-amount');
+    const amount = getInputNumber("#add-amount");
     const pin = getInputNumber("#password");
-    const totalAmount = parseFloat(
-      document.querySelector("#total-amount").innerText
-    );
+    const totalAmount = getInnerText("#total-amount");
 
     if (accountNumber.length < 11) {
       alert("Please provide a valid Account number");
@@ -42,64 +57,77 @@ document
 
     const totalNewBalance = amount + totalAmount;
 
-    document.querySelector("#total-amount").innerText = totalNewBalance;
+    setInnerText("#total-amount", totalNewBalance);
 
     // This clears the physical input box on the screen
     document.querySelector("#add-amount").value = "";
     document.querySelector("#bank-num").value = "";
     document.querySelector("#password").value = "";
-
   });
 
 // Cashout Logics
 
-document.querySelector("#cashout-withdraw-btn").addEventListener('click', function (e) {
-  e.preventDefault();
+document
+  .querySelector("#cashout-withdraw-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
 
-  const agentNum = getInputValue('#agent-num');
-  const cashoutAmount = getInputNumber("#cashout-amount")
-  const cashoutPin = getInputNumber("#cashout-pin");
-  const totalAmount = parseFloat(document.querySelector('#total-amount').innerText);
+    const agentNum = getInputValue("#agent-num");
+    const cashoutAmount = getInputNumber("#cashout-amount");
+    const cashoutPin = getInputNumber("#cashout-pin");
+    const totalAmount = getInnerText("#total-amount");
 
-  if (agentNum.length < 11) {
-    alert('Please provide a valid Agent number');
-    return;
-  }
+    if (agentNum.length < 11) {
+      alert("Please provide a valid Agent number");
+      return;
+    }
 
-  if (cashoutPin !== pinNumber) {
-    alert('Please provide a valid Pin number');
-    return;
-  }
+    if (cashoutPin !== pinNumber) {
+      alert("Please provide a valid Pin number");
+      return;
+    }
 
-  if (isNaN(cashoutAmount) || cashoutAmount <= 0) {
-    alert('Please enter a valid number');
-    return;
-  }
+    if (isNaN(cashoutAmount) || cashoutAmount <= 0) {
+      alert("Please enter a valid number");
+      return;
+    }
 
-  if (cashoutAmount > totalAmount) {
-    alert('Insufficient Balance');
-    return;
-  }
+    if (cashoutAmount > totalAmount) {
+      alert("Insufficient Balance");
+      return;
+    }
 
-  const newBalance = totalAmount - cashoutAmount;
-  document.querySelector('#total-amount').innerText = newBalance;
+    const newBalance = totalAmount - cashoutAmount;
+    setInnerText("#total-amount", newBalance);
 
-  document.querySelector("#cashout-amount").value = "";
-  document.querySelector("#cashout-pin").value = "";
-  document.querySelector("#agent-num").value = "";
-})
-
-
-
-
-
+    document.querySelector("#cashout-amount").value = "";
+    document.querySelector("#cashout-pin").value = "";
+    document.querySelector("#agent-num").value = "";
+  });
 
 // Toggle buttons
 document.querySelector("#add-btn").addEventListener("click", function () {
-  document.querySelector("#cashout-parent").style.display = "none";
-  document.querySelector("#add-money-parent").style.display = "block";
+  handleToggle("#add-money-parent");
 });
+
 document.querySelector("#cashout-btn").addEventListener("click", function () {
-  document.querySelector("#cashout-parent").style.display = "block";
-  document.querySelector("#add-money-parent").style.display = "none";
+  handleToggle("#cashout-parent");
 });
+
+document.querySelector("#transfer-btn").addEventListener("click", function () {
+  handleToggle("#transfer-parent");
+});
+
+document.querySelector("#bonus-btn").addEventListener("click", function () {
+  handleToggle("#get-bonus-parent");
+});
+
+document.querySelector("#bill-btn").addEventListener("click", function () {
+  handleToggle("#pay-bill-parent");
+});
+
+document
+  .querySelector("#transactions-btn")
+  .addEventListener("click", function () {
+    handleToggle("#transaction-parent");
+  });
